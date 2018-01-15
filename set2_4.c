5d208e295b2374ab93fa871bb28d35532ea77fea
HashRastogi <harshita.rastogi_cs15@gla.ac.in>
2018-01-15 23:49:13 +0530

    codekata beginner set 2 que 4

---
+++ set2_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+    int num1, num2;
+    scanf("%d%d", &num1, &num2);
+    if(num1%2 == 0)
+    num1++;
+    else
+    num1+=2;
+    for(int i=num1; i <= num2 ; i+= 2){
+        printf("%d ",i);
+    }
+}
