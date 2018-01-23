450fb426dfa243e9352accc45ed160702a9a71af
HashRastogi <harshita.rastogi_cs15@gla.ac.in>
2018-01-23 22:40:20 +0530

    codekata beginner set 4 que 3

---
+++ set4_3.c
@@ -0,0 +1,22 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int c = 0;
+    char str[1000];
+    fgets(str,1000,stdin);
+    
+    for(int i = 0; str[i]!='\0'; i++){
+        if(str[i] == ' ' )
+        c++;
+        
+    }
+    
+    printf("%d",c);
+    return 0;
+}
+
+
+
